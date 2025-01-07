2f06eab7fbe08d927b8f924840a5b73c720f3672
Chifrica <chikaonwunali2019@gmail.com>
2025-01-07 17:42:32 +0100

    add splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+
 import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontLoaded] = useFonts(  {
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-SemiLight": require('../assets/fonts/Rubik-Bold.ttf'),
+
+  }) 
+
+  useEffect( () => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }

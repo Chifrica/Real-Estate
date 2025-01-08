4f6cf0ab0be1a8553fbb5d2981348cec473c601a
Chifrica <chikaonwunali2019@gmail.com>
2025-01-08 16:24:33 +0100

    modified directories and updated screens

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,11 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to Real Estate2</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Real Estate</Text>
+
     </View>
   );
 }

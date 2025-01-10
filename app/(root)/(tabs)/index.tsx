5f986e5f8326a128aaa887207318da0a8ef83c69
Chifrica <chikaonwunali2019@gmail.com>
2025-01-10 09:22:05 +0100

    updated the index and Cards files

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -9,33 +9,48 @@ import { Card, FeaturedCards } from "@/components/Cards";
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full"/>
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Blessing</Text>
+      <ScrollView>
+        <View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full"/>
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                <Text className="text-base font-rubik-medium text-black-300">Blessing</Text>
+              </View>
             </View>
+            <Image source={icons.bell} className="size-6" />
           </View>
-          <Image source={icons.bell} className="size-6" />
-        </View>
 
-        <Search />
+          <Search />
 
-        <View className="my-5 ">
-          <View className="flex flex-row items-center justify-between px-5">
-            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
-            </TouchableOpacity> 
+          <View className="my-2 ">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity> 
+            </View>
+
+            <View className="flex flex-row gap-5 mt-5">
+                <FeaturedCards />
+                <FeaturedCards />
+              </View>
+          </View>
+
+          <View className="flex flex-row items-center justify-between mt-5">
+              <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity> 
+          </View>
+
+          <View className="flex flex-row gap-5">
+            <Card />
+            <Card />
           </View>
         </View>
-
-        <FeaturedCards />
-
-        <Card />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
